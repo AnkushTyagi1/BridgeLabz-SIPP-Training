dc24d35c2d1ff7980371acd077dba71ebae5bdad
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-13 11:21:24 +0530

    [Ankush Tyagi] Add find factors of a number using for loop.

--- Control Flow/Q26_FactorsFor.java
+++ Control Flow/Q26_FactorsFor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q26_FactorsFor {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         if (number > 0) {
