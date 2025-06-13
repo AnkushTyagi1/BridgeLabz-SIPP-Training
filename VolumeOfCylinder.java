efc8a2ddca4c2f5b2cad7cce4728ccf125f8a447
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-13 10:51:31 +0530

    [Ankush Tyagi] Add find the Volume of a Cylinder.

--- VolumeOfCylinder.java
+++ VolumeOfCylinder.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class VolumeOfCylinder {
-    public static void main(String[] args){
+    public static void main(String... args){
         Scanner sc=new Scanner(System.in);
         float radius=sc.nextFloat();
         float height= sc.nextFloat();
