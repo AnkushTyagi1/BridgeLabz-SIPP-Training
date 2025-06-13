2b5143ed6ebb7478fd0e45f9a24ce3f0a4de5182
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-13 11:13:44 +0530

    [Ankush Tyagi] Add Print the multiples of a number less than 100 and greater 0.

--- Control Flow/Q17_MultiplesBelow100.java
+++ Control Flow/Q17_MultiplesBelow100.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q17_MultiplesBelow100 {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         if (number > 0 && number < 100) {
