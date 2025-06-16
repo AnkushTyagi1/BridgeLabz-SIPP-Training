ca5a07acccbde5b9218f88c0b8ee7ab28e305bbc
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:59:25 +0530

    [Ankush Tyagi] Add SamAveragePCM

--- Programming-Elements/SamAveragePCM.java
+++ Programming-Elements/SamAveragePCM.java
@@ -1,5 +1,5 @@
 public class SamAveragePCM {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int maths = 94, physics = 95, chemistry = 96;
         double average = (maths + physics + chemistry) / 3.0;
         System.out.println("Sam’s average mark in PCM is " + average);
