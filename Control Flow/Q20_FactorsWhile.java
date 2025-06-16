456bcba0a4b485cb870ac21eecb37e0eca9a7ced
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:41:34 +0530

    [Ankush Tyagi] Add FactorsWhile

--- Control Flow/Q20_FactorsWhile.java
+++ Control Flow/Q20_FactorsWhile.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q20_FactorsWhile {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), i = 1;
         if (number > 0) {
