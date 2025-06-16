73c5fee5ccf81ef0e081feefe3a9e6926cb7315b
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:56:21 +0530

    [Ankush Tyagi] Add DoubleOpt

--- Programming-Elements/DoubleOpt.java
+++ Programming-Elements/DoubleOpt.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DoubleOpt {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter value of a: ");
         double a = input.nextDouble();
