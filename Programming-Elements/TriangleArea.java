8f34fa9c6aef5f19d9c18bc963dbcf2486fd98a0
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:59:52 +0530

    [Ankush Tyagi] Add TriangleArea

--- Programming-Elements/TriangleArea.java
+++ Programming-Elements/TriangleArea.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class TriangleArea {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter base in cm: ");
         double base = input.nextDouble();
