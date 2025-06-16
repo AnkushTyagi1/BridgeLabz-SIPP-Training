4b4e6469e24676a6a14c6ab31c7cf6a5ddcef3a2
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:48:22 +0530

    [Ankush Tyagi] Add CountdownFor

--- Control Flow/Q6_CountdownFor.java
+++ Control Flow/Q6_CountdownFor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q6_CountdownFor {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int counter = sc.nextInt();
         for (int i = counter; i >= 1; i--)
