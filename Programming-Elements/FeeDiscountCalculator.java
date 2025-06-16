ed11f6f5a0b5b11f94a4f0c0aa25f5f193d73d99
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:56:54 +0530

    [Ankush Tyagi] Add FeeDiscountCalculator

--- Programming-Elements/FeeDiscountCalculator.java
+++ Programming-Elements/FeeDiscountCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class FeeDiscountCalculator {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Student Fee (INR): ");
         double fee = input.nextDouble();
