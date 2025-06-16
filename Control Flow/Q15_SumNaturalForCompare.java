79ced55f9a0e6f3a35e082c3465e80fc7f3993f5
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:40:04 +0530

    [Ankush Tyagi] Add SumNaturalForCompare

--- Control Flow/Q15_SumNaturalForCompare.java
+++ Control Flow/Q15_SumNaturalForCompare.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q15_SumNaturalForCompare {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), sum = 0;
         if (n > 0) {
