d641115f02779ef4a02c5b96701eaef6eb906670
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:46:54 +0530

    [Ankush Tyagi] Add AbundantNumber

--- Control Flow/Q39_AbundantNumber.java
+++ Control Flow/Q39_AbundantNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q39_AbundantNumber {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt(), sum = 0;
         for (int i = 1; i < num; i++) {
