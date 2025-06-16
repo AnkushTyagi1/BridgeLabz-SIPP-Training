4cfd0630cc265efbcee450002ca93e627bf6bc1e
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:56:09 +0530

    [Ankush Tyagi] Add DistanceConverter

--- Programming-Elements/DistanceConverter.java
+++ Programming-Elements/DistanceConverter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DistanceConverter {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter distance in feet: ");
         double feet = input.nextDouble();
