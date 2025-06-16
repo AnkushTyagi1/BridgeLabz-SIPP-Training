d738186e10eeec92c04653a45c43889ba951a121
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:47:08 +0530

    [Ankush Tyagi] Add NumberSignCheck

--- Control Flow/Q3_NumberSignCheck.java
+++ Control Flow/Q3_NumberSignCheck.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q3_NumberSignCheck {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         if (number > 0)
