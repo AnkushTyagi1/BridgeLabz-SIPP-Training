1c42b7c5b9deedac000b387732f13509fddd317b
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:57:10 +0530

    [Ankush Tyagi] Add HarryAgeCalculator

--- Programming-Elements/HarryAgeCalculator.java
+++ Programming-Elements/HarryAgeCalculator.java
@@ -1,5 +1,5 @@
 public class HarryAgeCalculator {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int birthYear = 2000;
         int currentYear = 2024;
         int age = currentYear - birthYear;
