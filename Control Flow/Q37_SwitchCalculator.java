1f998501a8f540331089119c82458fc78beed1ae
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 08:23:56 +0530

    [Ankush Tyagi] Add design calculator using switch

--- Control Flow/Q37_SwitchCalculator.java
+++ Control Flow/Q37_SwitchCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q37_SwitchCalculator {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble(), b = sc.nextDouble();
         String op = sc.next();
