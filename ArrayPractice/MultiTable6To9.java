093b5b4803829e32d1d254c49b5fff24ec4562a9
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:22:43 +0530

    [Ankush Tyagi] Add MultiTable6To9

--- ArrayPractice/MultiTable6To9.java
+++ ArrayPractice/MultiTable6To9.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class MultiTable6To9 {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int[] multiplicationResult = new int[4];
