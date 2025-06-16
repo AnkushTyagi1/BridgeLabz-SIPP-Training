0e9edd60a91b731a90a27414fa2a1e993b6740f9
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:21:10 +0530

    [Ankush Tyagi] Add DigitFrequency

--- ArrayPractice/DigitFrequency.java
+++ ArrayPractice/DigitFrequency.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DigitFrequency {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         num = Math.abs(num);
