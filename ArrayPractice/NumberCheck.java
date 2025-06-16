1910d214124569066df0b54d026ad2c4f76c5acd
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:23:32 +0530

    [Ankush Tyagi] Add NumberCheck

--- ArrayPractice/NumberCheck.java
+++ ArrayPractice/NumberCheck.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class NumberCheck {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
