62e022916fc8e5095b555094fae90f9e344a8ab8
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:56:35 +0530

    [Ankush Tyagi] Add EarthVolume.java

--- Programming-Elements/EarthVolume.java
+++ Programming-Elements/EarthVolume.java
@@ -1,5 +1,5 @@
 public class EarthVolume {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         double radiusKm = 6378;
         double pi = Math.PI;
         double volumeKm3 = (4.0 / 3) * pi * Math.pow(radiusKm, 3);
