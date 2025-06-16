83f328957b806e0537c1e4d3ad0b546de801b088
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:24:49 +0530

    [Ankush Tyagi] Add VotingEligibility

--- ArrayPractice/VotingEligibility.java
+++ ArrayPractice/VotingEligibility.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class VotingEligibility {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int[] ages = new int[10];
         for (int i = 0; i < ages.length; i++) {
