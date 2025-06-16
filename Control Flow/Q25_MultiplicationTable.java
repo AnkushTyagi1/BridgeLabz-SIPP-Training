3b4f4b5d92fbe7d0eb98f3123639374d8c438ff4
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:42:52 +0530

    [Ankush Tyagi] Add MultiplicationTable

--- Control Flow/Q25_MultiplicationTable.java
+++ Control Flow/Q25_MultiplicationTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q25_MultiplicationTable {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         for (int i = 6; i <= 9; i++)
