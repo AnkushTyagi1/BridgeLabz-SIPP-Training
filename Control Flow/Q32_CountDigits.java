cc6d06909d2454fad05802dc4a2c41364a4ad14c
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 08:20:27 +0530

    [Ankush Tyagi] Add count the digits in a number

--- Control Flow/Q32_CountDigits.java
+++ Control Flow/Q32_CountDigits.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q32_CountDigits {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), count = 0;
         while (number != 0) {
