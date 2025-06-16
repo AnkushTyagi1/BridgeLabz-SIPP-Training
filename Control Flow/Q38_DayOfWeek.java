ec0abf598a9ec250ecdb834adf95bca8470a3d1b
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 08:24:41 +0530

    [Ankush Tyagi] Add Day of Week

--- Control Flow/Q38_DayOfWeek.java
+++ Control Flow/Q38_DayOfWeek.java
@@ -1,5 +1,5 @@
 public class Q38_DayOfWeek {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int m = Integer.parseInt(args[0]);
         int d = Integer.parseInt(args[1]);
         int y = Integer.parseInt(args[2]);
