edabe9f8e90f7828af513820ca625168c4a773e1
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:41:49 +0530

    [Ankush Tyagi] Add GreatestFactor

--- Control Flow/Q21_GreatestFactor.java
+++ Control Flow/Q21_GreatestFactor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q21_GreatestFactor {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), greatestFactor = 1;
         for (int i = number - 1; i >= 1; i--) {
