f725aa0d9039606f7a21f9184e47577dea67eae8
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:57:43 +0530

    [Ankush Tyagi] Add HeightConverter

--- Programming-Elements/HeightConverter.java
+++ Programming-Elements/HeightConverter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class HeightConverter {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your height in centimeters: ");
         double cm = input.nextDouble();
