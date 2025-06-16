c52dbdb0606b73db14d5ea70c994dc0f57879f3a
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:20:00 +0530

    [Ankush Tyagi] Add BMICalculator

--- ArrayPractice/BMICalculator.java
+++ ArrayPractice/BMICalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class BMICalculator {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         double[][] personData = new double[number][3];
