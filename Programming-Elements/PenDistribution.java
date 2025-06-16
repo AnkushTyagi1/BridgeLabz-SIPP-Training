58807fcefd7f5614192855070b56d34a27dcd052
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:58:26 +0530

    [Ankush Tyagi] Add PenDistribution

--- Programming-Elements/PenDistribution.java
+++ Programming-Elements/PenDistribution.java
@@ -1,5 +1,5 @@
 public class PenDistribution {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int totalPens = 14;
         int students = 3;
         int pensPerStudent = totalPens / students;
