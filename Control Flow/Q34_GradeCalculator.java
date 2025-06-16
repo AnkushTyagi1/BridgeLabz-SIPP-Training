7651053e312f5e08a836304cab642dee07fea966
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 08:21:44 +0530

    [Ankush Tyagi] Add calculate the grades using if else

--- Control Flow/Q34_GradeCalculator.java
+++ Control Flow/Q34_GradeCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q34_GradeCalculator {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int phy = sc.nextInt(), chem = sc.nextInt(), math = sc.nextInt();
         int total = phy + chem + math;
