d311e2ac7bdd98a0f21ed7644b5bb76ba52c4555
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:40:18 +0530

    [Ankush Tyagi] Add FizzBuzzWhile

--- Control Flow/Q16_FizzBuzzWhile.java
+++ Control Flow/Q16_FizzBuzzWhile.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q16_FizzBuzzWhile {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt(), i = 1;
         if (num > 0) {
