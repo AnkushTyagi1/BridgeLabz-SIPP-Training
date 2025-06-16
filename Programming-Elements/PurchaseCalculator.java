112ea0b74ec1366c7db17f9b6c8f09f5ee85b4ad
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:58:54 +0530

    [Ankush Tyagi] Add PurchaseCalculator

--- Programming-Elements/PurchaseCalculator.java
+++ Programming-Elements/PurchaseCalculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class PurchaseCalculator {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter unit price (INR): ");
         double unitPrice = input.nextDouble();
