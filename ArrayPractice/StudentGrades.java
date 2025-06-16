7cc2f1034a2f7bb5657da2a203733b3753c3596f
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:24:21 +0530

    [Ankush Tyagi] Add StudentGrades

--- ArrayPractice/StudentGrades.java
+++ ArrayPractice/StudentGrades.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class StudentGrades {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
 
         int students = sc.nextInt();
