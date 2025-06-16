72b880c3f053ef5031d507d1760e9ecc743ffcd4
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:55:36 +0530

    [Ankush Tyagi] Add ConvertKmToMiles

--- Programming-Elements/ConvertKmToMiles.java
+++ Programming-Elements/ConvertKmToMiles.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ConvertKmToMiles {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         double km;
         System.out.print("Enter distance in km: ");
