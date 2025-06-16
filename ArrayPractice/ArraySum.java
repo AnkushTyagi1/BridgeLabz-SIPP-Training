2accf8e290a900c4c65300f8bb769a1456c5a112
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:19:36 +0530

    [Ankush Tyagi] Add ArraySum.java

--- ArrayPractice/ArraySum.java
+++ ArrayPractice/ArraySum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ArraySum {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         double[] numbers = new double[10];
         double total = 0.0;
