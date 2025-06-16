0b96400af0d5fe149f9cb715fbf5f98379b0fdf6
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:25:05 +0530

    [Ankush Tyagi] Add YoungestAndTallestFriend

--- ArrayPractice/YoungestAndTallestFriend.java
+++ ArrayPractice/YoungestAndTallestFriend.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class YoungestAndTallestFriend {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
 
         String[] names = {"Amar", "Akbar", "Anthony"};
