424827829cb673b595b3bc02ea029e6d95317803
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 08:21:06 +0530

    [Ankush Tyagi] Add Armstrong number

--- Control Flow/Q33_ArmstrongNumber.java
+++ Control Flow/Q33_ArmstrongNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q33_ArmstrongNumber {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), original = number, sum = 0;
         while (number != 0) {
