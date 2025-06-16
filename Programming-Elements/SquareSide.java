1bd52d06b60fd66fe6e65f6f06df5157f248c916
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 14:59:38 +0530

    [Ankush Tyagi] Add SquareSide

--- Programming-Elements/SquareSide.java
+++ Programming-Elements/SquareSide.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class SquareSide {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter perimeter of square: ");
         double perimeter = input.nextDouble();
