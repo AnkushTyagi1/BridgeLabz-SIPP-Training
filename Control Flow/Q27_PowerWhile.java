d0a2948e7c4a6050fddf415f2674eb350e0aed28
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-16 10:43:22 +0530

    [Ankush Tyagi] Add PowerWhile

--- Control Flow/Q27_PowerWhile.java
+++ Control Flow/Q27_PowerWhile.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Q27_PowerWhile {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt(), power = sc.nextInt(), result = 1, counter = 0;
         while (counter < power) {
