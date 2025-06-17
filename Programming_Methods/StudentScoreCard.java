ec2f6549c5e14867986b12a9242e01ad3c75a8dc
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:24:08 +0530

    [Ankush Tyagi] Add StudentScoreCard

--- Programming_Methods/StudentScoreCard.java
+++ Programming_Methods/StudentScoreCard.java
@@ -34,7 +34,7 @@ public class StudentScoreCard {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students: ");
         int n = sc.nextInt();
