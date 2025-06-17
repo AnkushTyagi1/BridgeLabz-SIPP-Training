8f4b85f0f846211d098a4636431f2daec6c28b26
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:22:43 +0530

    [Ankush Tyagi] Add Quadratic

--- Programming_Methods/Quadratic.java
+++ Programming_Methods/Quadratic.java
@@ -13,7 +13,7 @@ public class Quadratic {
             return new double[]{};
         }
     }
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         double b = sc.nextDouble();
