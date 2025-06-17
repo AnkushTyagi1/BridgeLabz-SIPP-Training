2738ec2e840fc4fa1b28a6440ce9e82781cac1d6
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:23:50 +0530

    [Ankush Tyagi] Add SpringSeason

--- Programming_Methods/SpringSeason.java
+++ Programming_Methods/SpringSeason.java
@@ -2,7 +2,7 @@ public class SpringSeason {
     public static boolean isSpringSeason(int month, int day) {
         return (month == 3 && day >= 20) || (month == 4) || (month == 5) || (month == 6 && day <= 20);
     }
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int month = Integer.parseInt(args[0]);
         int day = Integer.parseInt(args[1]);
         if (isSpringSeason(month, day)) {
