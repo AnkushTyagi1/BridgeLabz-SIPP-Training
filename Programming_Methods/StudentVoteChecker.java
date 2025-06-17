f55c86c98417959fbd3b98f614ae3f2d4293cc5c
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:24:24 +0530

    [Ankush Tyagi] Add StudentVoteChecker

--- Programming_Methods/StudentVoteChecker.java
+++ Programming_Methods/StudentVoteChecker.java
@@ -4,7 +4,7 @@ public class StudentVoteChecker {
         if (age < 0) return false;
         return age >= 18;
     }
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int[] ages = new int[10];
         for (int i = 0; i < 10; i++) {
