b02a072a3b6a19533dc2fd8ea2df835742adf9d9
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:22:28 +0530

    [Ankush Tyagi] Add PointLineAnalysis

--- Programming_Methods/PointLineAnalysis.java
+++ Programming_Methods/PointLineAnalysis.java
@@ -11,7 +11,7 @@ public class PointLineAnalysis {
         return new double[]{m, b};
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         double x1 = sc.nextDouble(), y1 = sc.nextDouble();
         double x2 = sc.nextDouble(), y2 = sc.nextDouble();
