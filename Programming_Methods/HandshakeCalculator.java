42ab1366338940ec3c6fe53d4fe7d9778e7a48fc
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:20:30 +0530

    [Ankush Tyagi] Add HandshakeCalculator

--- Programming_Methods/HandshakeCalculator.java
+++ Programming_Methods/HandshakeCalculator.java
@@ -3,7 +3,7 @@ public class HandshakeCalculator {
     public static int calculateHandshakes(int numberOfStudents) {
         return (numberOfStudents * (numberOfStudents - 1)) / 2;
     }
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         int numberOfStudents = scanner.nextInt();
         int result = calculateHandshakes(numberOfStudents);
