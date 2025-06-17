1a525416fb99c44c0d453f19a6b99b15e5d63d25
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:18:42 +0530

    [Ankush Tyagi] Add CalendarDisplay

--- Programming_Methods/CalendarDisplay.java
+++ Programming_Methods/CalendarDisplay.java
@@ -46,7 +46,7 @@ public class CalendarDisplay {
         System.out.println();
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int month = sc.nextInt();
         int year = sc.nextInt();
