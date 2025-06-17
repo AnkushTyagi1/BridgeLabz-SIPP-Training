3ed72235b49bc9d62a4c2699ab43d28ac35f27cf
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-17 09:22:15 +0530

    [Ankush Tyagi] Add OTPGenerator

--- Programming_Methods/OTPGenerator.java
+++ Programming_Methods/OTPGenerator.java
@@ -14,7 +14,7 @@ public class OTPGenerator {
         return true;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         int[] otpArray = new int[10];
 
         for (int i = 0; i < 10; i++) {
