40fa7685dd0d5fc13a7d80ba38df1fdeacd84d14
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-18 09:54:07 +0530

    [Ankush Tyagi] WordLengthAnalysis

--- String_Practice/WordLengthAnalysis.java
+++ String_Practice/WordLengthAnalysis.java
@@ -62,7 +62,7 @@ public class WordLengthAnalysis {
         return new int[]{minIdx, maxIdx};
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the text: ");
         String input = sc.nextLine();
