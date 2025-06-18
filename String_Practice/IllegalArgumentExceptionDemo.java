18f8395ec99191f66c3e00a717446372e6e6b4eb
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-18 09:49:10 +0530

    [Ankush Tyagi] IllegalArgumentExceptionDemo

--- String_Practice/IllegalArgumentExceptionDemo.java
+++ String_Practice/IllegalArgumentExceptionDemo.java
@@ -19,7 +19,7 @@ public class IllegalArgumentExceptionDemo {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String text = sc.next();
