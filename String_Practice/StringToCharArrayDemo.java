25db3b557af509915ecd95283a65160e21e6e135
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-18 09:51:40 +0530

    [Ankush Tyagi] StringToCharArrayDemo

--- String_Practice/StringToCharArrayDemo.java
+++ String_Practice/StringToCharArrayDemo.java
@@ -17,7 +17,7 @@ public class StringToCharArrayDemo {
         return true;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.next();
