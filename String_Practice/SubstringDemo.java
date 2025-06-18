65d495878788c1afcb6e841d6ce0a1df56c6793f
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-18 09:52:18 +0530

    [Ankush Tyagi] SubstringDemo

--- String_Practice/SubstringDemo.java
+++ String_Practice/SubstringDemo.java
@@ -17,7 +17,7 @@ public class SubstringDemo {
         return true;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String text = sc.next();
