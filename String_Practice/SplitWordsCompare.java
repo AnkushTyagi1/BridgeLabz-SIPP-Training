6858be0b10f3c967bcb47df45eb79c5585cf1c75
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-18 09:50:41 +0530

    [Ankush Tyagi] SplitWordsCompare

--- String_Practice/SplitWordsCompare.java
+++ String_Practice/SplitWordsCompare.java
@@ -48,7 +48,7 @@ public class SplitWordsCompare {
         return true;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the text: ");
         String text = sc.nextLine();
