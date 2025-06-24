049e9044469608aad883d886cfd4aa660d116178
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-24 11:59:08 +0530

    [Ankush Tyagi] Add Circle

--- Java_Constructors/Circle.java
+++ Java_Constructors/Circle.java
@@ -6,7 +6,7 @@ public class Circle {
     public Circle(double radius){
         this.radius=radius;
     }
-    public static void main(String[] args){
+    public static void main(String... args){
         Circle c1=new Circle();
         Circle c2=new Circle(12.0);
     }
