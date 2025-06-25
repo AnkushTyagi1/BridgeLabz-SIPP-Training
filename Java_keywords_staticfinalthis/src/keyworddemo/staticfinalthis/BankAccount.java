ee432d626d1d16e0c8112a722d6852da1d62ac6a
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-06-25 11:41:29 +0530

    [Ankush Tyagi] Add BankAccount

--- Java_keywords_staticfinalthis/src/keyworddemo/staticfinalthis/BankAccount.java
+++ Java_keywords_staticfinalthis/src/keyworddemo/staticfinalthis/BankAccount.java
@@ -25,7 +25,7 @@ public class BankAccount {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		BankAccount ba1=new BankAccount(1001,"Bob");
 		BankAccount ba2=new BankAccount(1002,"Alice");
 		
