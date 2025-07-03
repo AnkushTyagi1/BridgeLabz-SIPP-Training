9b5bc1c23efa4b96a5e60b7407c239344ec31929
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-07-03 06:41:00 +0530

    [Ankush Tyagi] Add BankingSystemDemo

--- epicdemo/BankingSystemDemo.java
+++ epicdemo/BankingSystemDemo.java
@@ -81,7 +81,7 @@ class CurrentAccount extends BankAccount implements Loanable {
 
 //Main class
 public class BankingSystemDemo {
- public static void main(String[] args) {
+ public static void main(String... args) {
      List<BankAccount> accounts = new ArrayList<>();
      accounts.add(new SavingsAccount("SAV123", "John", 20000));
      accounts.add(new CurrentAccount("CUR456", "Jane", 60000));
