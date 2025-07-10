6d0b24627a10e2278760074c075b9c465e363bfe
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-07-10 08:52:31 +0530

    [Ankush Tyagi] Add Q6_HeapSort

--- SORTING ALGORITHMS/Q6_HeapSort.java
+++ SORTING ALGORITHMS/Q6_HeapSort.java
@@ -34,7 +34,7 @@ public class Q6_HeapSort {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of applicants: ");
         int n = sc.nextInt();
