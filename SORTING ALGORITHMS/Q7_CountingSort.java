aa98a92655908e6d3509fcf57942b421024b78c1
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-07-10 08:52:45 +0530

    [Ankush Tyagi] Add Q7_CountingSort

--- SORTING ALGORITHMS/Q7_CountingSort.java
+++ SORTING ALGORITHMS/Q7_CountingSort.java
@@ -23,7 +23,7 @@ public class Q7_CountingSort {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students: ");
         int n = sc.nextInt();
