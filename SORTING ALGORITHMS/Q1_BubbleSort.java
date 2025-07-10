37a8624dad27053692771a1826a0790da4430e3a
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-07-10 08:51:15 +0530

    [Ankush Tyagi] Add Q1_BubbleSort

--- SORTING ALGORITHMS/Q1_BubbleSort.java
+++ SORTING ALGORITHMS/Q1_BubbleSort.java
@@ -21,7 +21,7 @@ public class Q1_BubbleSort {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students: ");
         int n = sc.nextInt();
