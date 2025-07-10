7e632ebf5a97a29829732e5172c47e0ff9a18d4e
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-07-10 08:51:56 +0530

    [Ankush Tyagi] Add Q4_QuickSort

--- SORTING ALGORITHMS/Q4_QuickSort.java
+++ SORTING ALGORITHMS/Q4_QuickSort.java
@@ -29,7 +29,7 @@ public class Q4_QuickSort {
         return i + 1;
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of products: ");
         int n = sc.nextInt();
