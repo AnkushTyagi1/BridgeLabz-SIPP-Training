6d76670eb6e75409c4535ad5202dadb89d4ec14a
Ankush Tyagi <130550743+AnkushTyagi1@users.noreply.github.com>
2025-07-10 08:51:41 +0530

    [Ankush Tyagi] Add Q3_MergeSort

--- SORTING ALGORITHMS/Q3_MergeSort.java
+++ SORTING ALGORITHMS/Q3_MergeSort.java
@@ -34,7 +34,7 @@ public class Q3_MergeSort {
         while (j < n2) arr[k++] = R[j++];
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of books: ");
         int n = sc.nextInt();
